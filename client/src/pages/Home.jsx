461aef8baeddcda2a9b55bf76fff4d8ba50ca246
antonyjes <72809149@continental.edu.pe>
2024-07-15 12:32:02 -0500

    header and tabs options

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Layout from "./Layout";
+import { Button } from "@/components/ui/button";
+import { File, PlusCircle } from "lucide-react";
 
 const Home = () => {
     return(
-        <Layout>
-            <h1>Home</h1>
+        <Layout title={"Dashboard"}>
+            <Tabs variant="default" defaultValue="active">
+                <div className="flex">
+                    <TabsList>
+                        <TabsTrigger value="all">All</TabsTrigger>
+                        <TabsTrigger value="active">Active</TabsTrigger>
+                        <TabsTrigger value="completed">Completed</TabsTrigger>
+                    </TabsList>
+                    <div className="ml-auto flex items-center gap-2">
+                        <Button size="sm" variant="outline" className="h-7 gap-1">
+                            <File className="h-4 w-4" />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Export</span>
+                        </Button>
+                        <Button size="sm" className="h-7 gap-1">
+                            <PlusCircle className="h-4 w-4" />
+                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                                Add Task
+                            </span>
+                        </Button>
+                    </div>
+                </div>
+            </Tabs>
         </Layout>
     )
 }
