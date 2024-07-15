235e6a9c1af32f77112738a26a09e0cd4b4f7b69
antonyjes <72809149@continental.edu.pe>
2024-07-15 12:32:13 -0500

    main layout

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -59,9 +59,7 @@ const Layout = ({ children, title }) => {
         <div className="mx-auto grid w-full max-w-6xl gap-2">
           <h1 className="text-3xl font-semibold">{title}</h1>
         </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          {children}
-        </div>
+        {children}
       </main>
     </div>
   );
