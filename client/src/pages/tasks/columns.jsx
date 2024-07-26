532c464d2c64451fd87b795ae6ad3475e67afb43
antonyjes <72809149@continental.edu.pe>
2024-07-26 12:09:35 -0500

    columns for datatable

---
+++ client/src/pages/tasks/columns.jsx
@@ -0,0 +1,22 @@
+export const columns = [
+    {
+        accessorKey: 'id',
+        header: 'Id',
+    },
+    {
+        accessorKey: 'taskName',
+        header: 'Task Name',
+    },
+    {
+        accessorKey: 'status',
+        header: 'Status',
+    },
+    {
+        accessorKey: 'date',
+        header: 'Date',
+    },
+    {
+        id: 'actions',
+        cell: null,
+    }
+]
