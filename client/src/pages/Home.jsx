5b148821af28e4244881b3b2877990a2b4ca93f2
antonyjes <72809149@continental.edu.pe>
2024-07-26 12:08:12 -0500

    home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,38 @@
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useEffect, useState } from "react";
 import Layout from "./Layout";
-import { Button } from "@/components/ui/button";
-import { File, PlusCircle } from "lucide-react";
+import { TaskClient } from "./tasks/client";
+import ModalTask from "./Modal";
 
 const Home = () => {
+    const [tasks, setTasks] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+    const getTasks = async () => {
+        const response = await fetch("http://localhost:3000/api/tasks");
+
+        const data = await response.json();
+        setTasks(data);
+    }
+        
+    useEffect(() => {
+        getTasks();
+    }, [])
+
     return(
-        <Layout title={"Dashboard"}>
-            <Tabs variant="default" defaultValue="active">
-                <div className="flex">
-                    <TabsList>
-                        <TabsTrigger value="all">All</TabsTrigger>
-                        <TabsTrigger value="active">Active</TabsTrigger>
-                        <TabsTrigger value="completed">Completed</TabsTrigger>
-                    </TabsList>
-                    <div className="ml-auto flex items-center gap-2">
-                        <Button size="sm" variant="outline" className="h-7 gap-1">
-                            <File className="h-4 w-4" />
-                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Export</span>
-                        </Button>
-                        <Button size="sm" className="h-7 gap-1">
-                            <PlusCircle className="h-4 w-4" />
-                            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                                Add Task
-                            </span>
-                        </Button>
-                    </div>
-                </div>
-            </Tabs>
-        </Layout>
+        <>
+            <Layout title={"Dashboard"}>
+                <TaskClient data={tasks} setShowModal={setShowModal} />
+            </Layout>
+            {
+                showModal && (
+                    <ModalTask
+                        showModal={showModal}
+                        setShowModal={setShowModal}
+                        operation={"Create"}
+                    />
+                )
+            }
+        </>       
     )
 }
 
