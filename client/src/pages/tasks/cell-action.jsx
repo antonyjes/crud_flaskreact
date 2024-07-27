031f59eaad6812b6e4d832d30a3f52f4c8425616
antonyjes <72809149@continental.edu.pe>
2024-07-26 19:46:35 -0500

    toast message

--- client/src/pages/tasks/cell-action.jsx
+++ client/src/pages/tasks/cell-action.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 import ModalTask from "../Modal";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { AlertModal } from "@/components/alert-modal";
+import { toast } from "react-toastify";
 
 export const CellAction = ({ data }) => {
   const [showModal, setShowModal] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   const onDelete = async () => {
-    await fetch(`http://localhost:3000/api/tasks/${data.id}/delete`, {
+    await fetch(`http://localhost:3003/tasks/${data.id}/delete`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
     });
 
+    toast.success("Task deleted successfully");
     setShowAlert(false);
   };
 
@@ -29,30 +38,30 @@ export const CellAction = ({ data }) => {
           currentTask={data}
         />
       )}
-      {
-        showAlert && (
-            <div>
-                Alert
-            </div>
-        )
-      }
+      {showAlert && (
+        <AlertModal
+          showAlert={showAlert}
+          setShowAlert={setShowAlert}
+          onConfirm={onDelete}
+        />
+      )}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-            </Button>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem>
-                <Edit className="mr-2 h-4 w-4" />
-                Update
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setShowAlert(true)}>
-                <Trash className="mr-2 h-4 w-4" />
-                Delete
-            </DropdownMenuItem>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => setShowModal(true)}>
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setShowAlert(true)}>
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </>
